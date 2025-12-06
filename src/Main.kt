b5e757046f2fc8336fcf567b6da575672eb1b8c6
cand6 <fjhdjdj876@gmail.com>
2025-12-06 15:29:15 +0300

    5 лаба

--- src/Main.kt
+++ src/Main.kt
@@ -1,129 +1,138 @@
-// ЗАДАНИЕ 1
-fun showGameLogo() {
-    println("=== EPIC QUEST ===")
-}
+import kotlin.math.PI
 
-// ЗАДАНИЕ 2
-fun showEnemy(name: String, level: Int) {
-    println("Враг: $name (Уровень: $level)")
-}
+// Задание 1. Класс Weapon
+class Weapon {
+    var name = "Оружие"
+    var damage = 10
+    var durability = 100
+    var type = "Обычное"
 
-// ЗАДАНИЕ 3
-fun spawnEnemy(name: String, health: Int = 100, isBoss: Boolean = false) {
-    println("Враг: $name (Здоровье: $health HP${if (isBoss) " | БОСС!" else ""})")
-}
+    fun showInfo() {
+        println("Название: $name | Урон: $damage | Прочность: $durability | Тип: $type")
+    }
 
-// ЗАДАНИЕ 5
-fun calculateXP(level: Int): Int {
-    return level * 1000
-}
+    fun use() {
+        if (durability <= 0) {
+            println("Оружие сломано!")
+            return
+        }
+        durability -= 10
+        if (durability <= 0) {
+            durability = 0
+            println("Оружие сломано!")
+        } else {
+            println("Оружие использовано. Прочность: $durability")
+        }
+    }
 
-// ЗАДАНИЕ 6
-val inventory = listOf("Меч", "Щит", "Лук")
-
-fun checkInventory(item: String): Boolean {
-    return item in inventory
-}
-
-// ЗАДАНИЕ 7
-fun castSpell() {
-    println("Каст случайного заклинания!")
-}
-
-fun castSpell(spell: String) {
-    println("Каст заклинания $spell!")
-}
-
-fun castSpell(spell: String, power: Int) {
-    println("Каст $spell с силой $power!")
-}
-
-// ЗАДАНИЕ 8
-fun createWeapon(name: String): String {
-    return "Оружие: $name"
-}
-
-fun createWeapon(name: String, damage: Int): String {
-    return "Оружие: $name (Урон: $damage)"
-}
-
-fun createWeapon(damage: Int, isMagic: Boolean): String {
-    val type = if (isMagic) "Магическое" else "Обычное"
-    return "$type оружие (Урон: $damage)"
-}
-
-// ЗАДАНИЕ 9
-fun heal() {
-    println("Лечение: +10 HP")
-}
-
-fun heal(amount: Int) {
-    println("Лечение: +$amount HP")
-}
-
-fun heal(amount: Int, isPotion: Boolean) {
-    if (isPotion) {
-        println("Выпито зелье (+$amount HP)")
-    } else {
-        println("Заклинание лечения (+$amount HP)")
+    fun upgrade(bonus: Int) {
+        damage += bonus
+        println("Урон увеличен на $bonus. Теперь урон: $damage")
     }
 }
 
-// MAIN
+// Задание 2. Класс Student
+class Student(var name: String, var age: Int) {
+    init {
+        println("Создан студент")
+    }
+
+    fun introduce() {
+        println("Меня зовут $name, мне $age лет")
+    }
+
+    fun haveBirthday() {
+        age++
+        println("$name празднует день рождения! Теперь ему/ей $age лет")
+    }
+}
+
+// Задание 3. Класс Group
+class Group(val groupName: String) {
+    private val students = mutableListOf<Student>()
+
+    init {
+        println("Создана группа")
+    }
+
+    fun addStudent(student: Student) {
+        students.add(student)
+        println("Студент ${student.name} добавлен в группу $groupName")
+    }
+
+    fun listStudents() {
+        println("Студенты группы $groupName:")
+        for (student in students) {
+            student.introduce()
+        }
+    }
+
+    fun celebrateBirthdays() {
+        println("В группе $groupName празднуют дни рождения!")
+        for (student in students) {
+            student.haveBirthday()
+        }
+    }
+}
+
+// Задание 4. Класс GeometryCalculator
+class GeometryCalculator {
+    fun rectangleInfo(width: Int, height: Int) {
+        val perimeter = 2 * (width + height)
+        val area = width * height
+        println("Прямоугольник $width x $height:")
+        println("Периметр: $perimeter")
+        println("Площадь: $area")
+        if (perimeter == area) {
+            println("Удивительно! Периметр равен площади!")
+        }
+    }
+
+    fun triangleInfo(a: Int, b: Int, c: Int) {
+        val perimeter = a + b + c
+        val isRight = (a * a + b * b == c * c) ||
+                (a * a + c * c == b * b) ||
+                (b * b + c * c == a * a)
+        println("Треугольник со сторонами: $a, $b, $c")
+        if (isRight) {
+            println("Это прямоугольный треугольник!")
+        } else {
+            println("Это не прямоугольный треугольник")
+        }
+        println("Периметр: $perimeter")
+    }
+
+    fun circleInfo(radius: Double) {
+        val circumference = 2 * PI * radius
+        val area = PI * radius * radius
+        println("Окружность с радиусом $radius:")
+        println("Длина окружности: $circumference")
+        println("Площадь круга: $area")
+    }
+}
+
 fun main() {
-    // Задание 1
-    showGameLogo()
-    showGameLogo()
+    println("Оружие")
+    val weapon = Weapon()
+    weapon.showInfo()
+    weapon.upgrade(5)
+    weapon.use()
 
-    println()
+    println("\nСтудент")
+    val student = Student("Данила", 20)
+    student.introduce()
+    student.haveBirthday()
 
-    // Задание 2
-    showEnemy("Гоблин", 5)
-    showEnemy("Орк", 10)
+    println("\nГруппа")
+    val group = Group("ИСП-233")
+    group.addStudent(student)
+    group.addStudent(Student("Денис", 20))
+    group.listStudents()
+    group.celebrateBirthdays()
 
-    println()
-
-    // Задание 3
-    spawnEnemy("Скелет")
-    spawnEnemy("Дракон", isBoss = true)
-
-    println()
-
-    // Задание 4
-    spawnEnemy("Зомби", 300)
-    spawnEnemy("Король Тьмы", 500, true)
-
-    println()
-
-    // Задание 5
-    val xpForLevel5 = calculateXP(5)
-    println("Опыт для 5 уровня: $xpForLevel5")
-
-    println()
-
-    // Задание 6
-    println("Есть ли 'Меч' в инвентаре? ${checkInventory("Меч")}")
-    println("Есть ли 'Зелье' в инвентаре? ${checkInventory("Зелье")}")
-
-    println()
-
-    // Задание 7
-    castSpell()
-    castSpell("Огненный шар")
-    castSpell("Ледяная стрела", 25)
-
-    println()
-
-    // Задание 8
-    println(createWeapon("Меч"))
-    println(createWeapon("Лук", 15))
-    println(createWeapon(30, true))
-
-    println()
-
-    // Задание 9
-    heal()
-    heal(50)
-    heal(100, true)
-    heal(75, false)
+    println("\nГеометрия")
+    val calc = GeometryCalculator()
+    calc.rectangleInfo(4, 5)
+    calc.triangleInfo(3, 4, 5)
+    calc.circleInfo(5.0)
 }
