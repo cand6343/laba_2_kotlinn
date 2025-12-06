9e72e9aa8fa953d7a11ea14fe01aca3d1bf48b34
cand6 <fjhdjdj876@gmail.com>
2025-12-06 14:49:27 +0300

    3 лаба 4

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,24 @@
-fun main() {
-    print("Введите число N: ")
-    val n = readln().toInt()
+import kotlin.random.Random
 
-    var i = n
-    while (i >= 1) {
-        println(i)
-        i--
+fun main() {
+    val lower = "abcdefghijklmnopqrstuvwxyz"
+    val upper = lower.uppercase()
+    val digits = "0123456789"
+    val symbols = "!@#$%^&*()_+-="
+    val allChars = lower + upper + digits + symbols
+
+    val length = Random.nextInt(8, 17)
+    val password = CharArray(length)
+
+    password[0] = upper[Random.nextInt(upper.length)]
+    password[1] = lower[Random.nextInt(lower.length)]
+    password[2] = digits[Random.nextInt(digits.length)]
+    password[3] = symbols[Random.nextInt(symbols.length)]
+
+    for (i in 4 until length) {
+        password[i] = allChars[Random.nextInt(allChars.length)]
     }
+
+    password.shuffle()
+    println("Сгенерированный пароль: ${String(password)}")
 }
