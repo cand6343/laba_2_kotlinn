ff523ec6c703a9bf6afb821fbe4c2268a92a184b
cand6 <fjhdjdj876@gmail.com>
2025-12-06 13:40:20 +0300

    5 задание

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,6 @@
 fun main() {
-    print("Введите один символ: ")
-    val ch = readln().first()
-
-    val type = when (ch) {
-        in '0'..'9' -> "Цифра"
-        in 'a'..'z', in 'A'..'Z' -> "Буква латинского алфавита"
-        ' ' -> "Пробел"
-        else -> "Спецсимвол"
+    val number = 5
+    repeat(10) { i ->
+        println("$number × ${i + 1} = ${number * (i + 1)}")
     }
-
-    println("Это $type.")
 }
