43559065d839f0dedf61a769febb90f8c9d373dd
cand6 <fjhdjdj876@gmail.com>
2025-12-06 14:51:08 +0300

    3 лаба 8

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,15 @@
 import kotlin.random.Random
 
 fun main() {
-    println("10 бросков шестигранного кубика:")
+    val num1 = Random.nextInt(0, 6)
+    val num2 = Random.nextInt(0, 6)
+    val num3 = Random.nextInt(0, 6)
 
-    for (i in 1..10) {
-        val dice = Random.nextInt(1, 7)
-        println("Бросок $i: $dice")
+    println("Слот-машина: [$num1] [$num2] [$num3]")
+
+    if (num1 == num2 && num2 == num3) {
+        println("Джекпот! 🎰")
+    } else {
+        println("Попробуйте ещё раз")
     }
 }
